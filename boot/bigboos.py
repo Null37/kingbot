1dc8b0ce37d056d07b68126af93ebcec8016ff3e
Axylum <cedric.legrand@gmail.com>
2023-11-17 09:16:07 +0100

    Python source code formatting and optimisation

--- boot/bigboos.py
+++ boot/bigboos.py
@@ -1,30 +1,25 @@
 import requests
-import urllib3
 from bs4 import BeautifulSoup
-import os
-import os.path
-import base64 
-from selenium import webdriver
-from os.path  import basename
-from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoSuchElementException
-import time
 
 serch = "poster attack on titan"
 sleep = 4
-#chromrdriver = "C:\Program Files (x86)\chromedriver.exe"
-#os.environ["webdriver.chrome.driver"] = chromrdriver
-#driver = webdriver.Chrome(chromrdriver)
-#driver.maximize_window()
+# chromrdriver = "C:\Program Files (x86)\chromedriver.exe"
+# os.environ["webdriver.chrome.driver"] = chromrdriver
+# driver = webdriver.Chrome(chromrdriver)
+# driver.maximize_window()
 number_page = '1'
 url = 'https://www.redbubble.com/shop/?' + 'page=' + number_page + '&query=' + serch
 href = []
+
+
 def get_click_href(url):
-	i = 0
-	r = requests.get(url) 
-	soup = BeautifulSoup(r.text, 'lxml')
-	site = soup.find_all('a', {'class' : 'styles__link--2sYi3'})
-	for s in site:
-		href.append(s['href'])
-	print(href[0])
+    i = 0
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'lxml')
+    site = soup.find_all('a', {'class': 'styles__link--2sYi3'})
+    for s in site:
+        href.append(s['href'])
+    print(href[0])
+
+
 get_click_href(url)
--- boot/kingbot.py
+++ boot/kingbot.py
@@ -1,64 +1,68 @@
-import requests
-import urllib3
-from bs4 import BeautifulSoup
 import os
 import os.path
-from selenium import webdriver
-from os.path  import basename
-from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoSuchElementException
 import time
+
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 search = "poster attack on titan"
 sleep = 5
 number_page = '1'
-#url = 'https://www.redbubble.com/shop/?' + 'page=' + number_page + search
-def scrol_loop():
-	i = 0
-	while i < 15:
-		time.sleep(sleep)
-		driver.find_element_by_tag_name('body').send_keys(Keys.PAGE_DOWN)
-		i += 1
-def scrol_and_download(url, number_of_the_page):
-	chromrdriver = "C:\Program Files (x86)\chromedriver.exe"
-	os.environ["webdriver.chrome.driver"] = chromrdriver
-	driver = webdriver.Chrome(chromrdriver)
-	driver.get(url)
-	driver.maximize_window()
-	scrol_loop()
-	num = str(number_of_the_page)
-	file_name = search + num + ".html" 
-	file_exists = os.path.isfile(file_name)
-	html = driver.page_source
-	path = os.getcwd()
-	if not os.path.exists(search + num):
-		os.makedirs(search + num)
-	else :
-		pass
-	if file_exists:
-		with open(file_name, 'w', newline='', encoding="utf-8") as file:
-			file.write(html)
-	else:
-		with open(file_name, 'x', newline='', encoding="utf-8") as file:
-			file = open(file_name, 'w', newline='', encoding="utf-8") 
-			file.write(html)
-	file.close()
 
-	driver.close()
-	with open(file_name, newline='', encoding="utf-8") as file:
-		contents = file.read()	
-		soup = BeautifulSoup(contents, 'lxml')
-		images = soup.find_all('img', {'class': 'styles__image--2CwxX styles__productImage--3ZNPD styles__rounded--1lyoH styles__fluid--3dxe-'})
-	path = search + num
-	for image in images:
-		name = image['alt']
-		link = image.get('src')
-		with open(os.path.join(path,name.replace(' ', '-').replace('/', '').replace('|','').replace('?', '-').replace('*','--0--')) + '.jpg', 'wb') as f :
-			f.write(requests.get(link).content)
+
+# url = 'https://www.redbubble.com/shop/?' + 'page=' + number_page + search
+def scrol_loop():
+    for _ in range(15):
+        time.sleep(sleep)
+        driver.find_element_by_tag_name('body').send_keys(Keys.PAGE_DOWN)
+
+
+def scrol_and_download(url, number_of_the_page):
+    chromrdriver = "C:\Program Files (x86)\chromedriver.exe"
+    os.environ["webdriver.chrome.driver"] = chromrdriver
+    driver = webdriver.Chrome(chromrdriver)
+    driver.get(url)
+    driver.maximize_window()
+    scrol_loop()
+    num = str(number_of_the_page)
+    file_name = search + num + ".html"
+    file_exists = os.path.isfile(file_name)
+    html = driver.page_source
+    path = os.getcwd()
+    if not os.path.exists(search + num):
+        os.makedirs(search + num)
+    if file_exists:
+        with open(file_name, 'w', newline='', encoding="utf-8") as file:
+            file.write(html)
+    else:
+        with open(file_name, 'x', newline='', encoding="utf-8") as file:
+            file = open(file_name, 'w', newline='', encoding="utf-8")
+            file.write(html)
+    file.close()
+
+    driver.close()
+    with open(file_name, newline='', encoding="utf-8") as file:
+        contents = file.read()
+        soup = BeautifulSoup(contents, 'lxml')
+        images = soup.find_all('img', {
+            'class': 'styles__image--2CwxX styles__productImage--3ZNPD styles__rounded--1lyoH styles__fluid--3dxe-'})
+    path = search + num
+    for image in images:
+        name = image['alt']
+        link = image.get('src')
+        with open(os.path.join(path,
+                               name.replace(' ', '-').replace('/', '').replace('|', '').replace('?', '-').replace('*',
+                                                                                                                  '--0--')) + '.jpg',
+                  'wb') as f:
+            f.write(requests.get(link).content)
+
+
 a = 1
 while a <= 5:
-	url = 'https://www.redbubble.com/shop/?' + 'page=' + str(a) + search
-	scrol_and_download(url, a)
-	a += 1
-#print(url)
+    url = 'https://www.redbubble.com/shop/?' + 'page=' + str(a) + search
+    scrol_and_download(url, a)
+    a += 1
+# print(url)
 print(a)
